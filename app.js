a1979dea1d0f7facea13effbb69ceaff80a97d68
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-16 10:24:05 +0200

    update

--- app.js
+++ app.js
@@ -4,50 +4,49 @@ let nextPlayer = 0;
 let symbols = ['X', 'O'];
 
 const winningCombinations = [
-    ['00', '01', '02'],
-    ['10', '11', '12'],
-    ['20', '21', '22'],
-    ['00', '10', '20'],
-    ['01', '11', '21'],
-    ['02', '12', '22'],
-    ['00', '11', '22'],
-    ['02', '11', '20'],
+    ['00', '01', '02'], // top row
+    ['10', '11', '12'], // middle row
+    ['20', '21', '22'], // bottom row
+    ['00', '10', '20'], // left column
+    ['01', '11', '21'], // middle column
+    ['02', '12', '22'], // right column
+    ['00', '11', '22'], // top-left to bottom-right diagonal
+    ['02', '11', '20']  // top-right to bottom-left diagonal
 ];
-
-let player0 = [];
-let player1 = [];
 
 let gameState = [[], []];
 
 cellDivs.forEach( cellDiv => {
-
+    
     cellDiv.addEventListener('click', e => {
 
-        if ( !e.target.innerText) {
+        if ( !e.target.innerText ) {
 
             const move = e.target.dataset.y + e.target.dataset.x;
             gameState[nextPlayer].push(move);
 
             e.target.innerText = symbols[nextPlayer];
 
-            isGameOver(gameState[nextPlayer]);
+            console.log(isGameOver(gameState[nextPlayer]));
 
             nextPlayer = Number(!nextPlayer);
 
-            console.log(gameState)
         }
+
     });
+
 });
 
 function isGameOver ( moves ) {
+
     let isGameOver = false;
 
-    winningCombinations.forEach(c => {
-        if (c.every(m => moves.includes(m))) {
-            isGameOver = true
+    winningCombinations.forEach( c => {
+        if ( c.every(m => moves.includes(m)) ) {
+            isGameOver = true;
         }
     });
-
-    return isGameOver
+    
+    return isGameOver;
 
 }
